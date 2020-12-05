f4078d5580c7a7325f70915924b45c91a926717e
sandgate279 <41950443+sandgate279@users.noreply.github.com>
2020-12-05 20:08:14 +1100

    Update ANZ.sql

--- ANZ.sql
+++ ANZ.sql
@@ -1,17 +1,11 @@
-select sum(b.balance) as Total_balance ,b.customer_id,b.age from [WK9].[dbo].[ANZ] as b
+Select SUM(x.amount) as Annual_salary,SUM(x.balance)as Total_balance,x.age,x.customer_id
+FROM
+[WK9].[dbo].[ANZ] as x
+where 
+x.customer_id in
 
-group by 
-b.customer_id,b.long_lat,b.age
-order by 
-
-b.customer_id desc
-
-
-select sum(b.amount) as Annual_salary ,b.customer_id from [WK9].[dbo].[ANZ] as b
-where b.txn_description='PAY/SALARY'
-
-group by 
-b.customer_id
-order by 
-
-b.customer_id desc
+(select customer_id from [WK9].[dbo].[ANZ]
+where
+txn_description='PAY/SALARY')
+group by
+x.customer_id,x.age
